a233aef1217370a30eff78fca00a36cff416d599
hiromisanui <hiromisanui@gmail.com>
2020-06-27 04:35:13 +0900

    add todolist

---
+++ src/components/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-container>
+    <b-row :key="index" v-for="(todo, index) in todos" class="mt-2">
+      <b-col cols="6" :class="[{line: todo.complete}, 'text-left']">
+        <h5>{{ todo.text }}</h5>
+      </b-col>
+      <b-col cols="6" class="text-right">
+        <b-button
+          @click="handleCompleteTodo(index)"
+          :variant="todo.complete ? '' : 'success'">
+          {{ todo.complete ? '完了' : '未完了'}}
+        </b-button>
+        <b-button
+          @click="handleDeleteTodo(index)" variant="danger">
+          削除
+        </b-button>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+  export default {
+    name:'TodoList',
+    props:['todos'],
+    methods:{
+      handleDeleteTodo(index){
+        this.$emit('handleDeleteTodo', index)
+      },
+      handleCompleteTodo(index){
+        this.$emit('handleCompleteTodo', index)
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .line {
+    text-decoration: line-through;
+  }
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <todo-form @handleAddTodo="handleAddTodo" />
+    <todo-list
+      :todos="todos"
+      @handleDeleteTodo="handleDeleteTodo"
+      @handleCompleteTodo="handleCompleteTodo" />
+  </div>
+</template>
+
+<script>
+  import TodoForm from '@/components/TodoForm';
+  import TodoList from '@/components/TodoList';
+
+  export default {
+    name: "Todo",
+    components:{
+      TodoForm,
+      TodoList,
+    },
+    data(){
+      return{
+        todos:[],
+      }
+    },
+    methods:{
+      handleAddTodo(value){
+        if(value){
+          this.todos.unshift({text: value, complete: false});
+        }
+      },
+      handleDeleteTodo(index){
+        this.todos.splice(index, 1);
+      },
+      handleCompleteTodo(index){
+        this.todos[index].complete = !this.todos[index].complete;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
